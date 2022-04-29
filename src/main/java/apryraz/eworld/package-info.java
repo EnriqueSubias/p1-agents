57c72a0b3b17fa1e3a079d565a2c32d3bac14c45
EnriqueSubias <48204227+EnriqueSubias@users.noreply.github.com>
2022-04-29 23:06:30 +0200

    Final Commit

--- src/main/java/apryraz/eworld/package-info.java
+++ src/main/java/apryraz/eworld/package-info.java
@@ -1,8 +1,7 @@
 /**
-* This package contains an implementation of the Envelope World,
-* where an agent has to find the location of the envelopes using
-* the detector sensor to get information about the world.
-*
-**/
+ * This package contains an implementation of the Envelope World,
+ * where an agent has to find the location of the envelopes using
+ * the detector sensor to get information about the world.
+ **/
 
 package apryraz.eworld;
